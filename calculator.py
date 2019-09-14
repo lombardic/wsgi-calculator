3ea13e352caac857b00292c77ecc1a73fc918b3c
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-09-13 22:37:30 -0700

    Adding updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -41,44 +43,143 @@ To submit your homework:
 
 """
 
+import traceback
+
+def intro(*args):
+    """Introduces the calculator to the user"""
+    page = """
+        <html>
+        <h1>WSGI Calculator</h1>
+        <body>
+            This page provides the user with the ability to add, subtract,
+            multiply, and divide two numbers.
+
+        <h2>Instructions</h2>
+        In the url, please enter the arithmetic operation you would like to perform
+        followed by the two values involved in the operation. The available
+        arithmetic operations are as follows:<br></br>
+        add<br></br>
+        subtract<br></br>
+        multiply<br></br>
+        divide
+        <h3>Example</h3>
+        Url Entry:  /add/3/4/<br></br>
+        Result: 3 + 4 = 7
+        </body>
+        </html>
+    """
+    return page
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    sum_num = 0
+    operands = []
+    try:
+        for i in range(0, len(args)):
+            sum_num = sum_num + int(args[i])
+            operands.append(f'{args[i]}')
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+        operand_string = ' + '.join(operands)
+        return f'{operand_string} = {sum_num}'
+    except IndexError:
+        raise IndexError
 
-    return sum
+def subtract(*args):
+    """Returns a STRING with the difference of the arguments."""
+    difference = int(args[0])
+    operands = [f'{args[0]}']
+    try:
+        for i in range(1, len(args)):
+            difference = difference - int(args[i])
+            operands.append(f'{args[i]}')
 
-# TODO: Add functions for handling more arithmetic operations.
+        operand_string = ' - '.join(operands)
+        return f'{operand_string} = {difference}'
+    except IndexError:
+        return IndexError
+
+def multiply(*args):
+    """Returns a STRING with the product of the agruments."""
+    product = int(args[0])
+    operands = [f'{args[0]}']
+    try:
+        for i in range(1, len(args)):
+            product = product * int(args[i])
+            operands.append(f'{args[i]}')
+
+        operand_string = ' * '.join(operands)
+        return f'{operand_string} = {product}'
+    except IndexError:
+        raise IndexError
+
+def divide(*args):
+    """Returns a STRING with the quotient of the arguments."""
+    quotient = int(args[0])
+    operands = [f'{args[0]}']
+    try:
+        for i in range(1, len(args)):
+            try:
+                quotient = quotient / int(args[i])
+                operands.append(f'{args[i]}')
+            except ZeroDivisionError:
+                raise ZeroDivisionError
+
+        operand_string = ' / '.join(operands)
+        return f'{operand_string} = {quotient}'
+    except IndexError:
+        raise IndexError
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    path = path.strip('/').split('/')
+    funcs = {'': intro,
+             'add': add,
+             'subtract': subtract,
+             'multiply': multiply,
+             'divide': divide
+             }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+      raise NameError
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+        status = "400 Bad Request"
+        body = "<h1>Invalid Result. A number can't be divided by zero.</h1>"
+    except IndexError:
+        status = "400 Bad Request"
+        body = "<h1>Insufficient Operands"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(("Content-length", str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
